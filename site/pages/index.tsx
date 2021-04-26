d5785c6e6a37fb425b44c69cd198f7c69f1e5b1f
Nick Baker <ndbaker1@outlook.com>
2021-04-26 18:00:08 -0500

    add typescript and eslint to site project

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
-import styles from '../styles/Home.module.css'
-import { w3cwebsocket as W3CWebSocket } from "websocket"
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
 
 const socket = new W3CWebSocket('ws://127.0.0.1:8000/ws/f31c43d9dbf34a538d1a4d9322e7dde9')
 //curl -X POST 'http://localhost:8000/register' -H 'Content-Type: application/json' -d '{ "user_id": 1 }
-export default function Home() {
+
+export default function Home(): JSX.Element {
   useEffect(() => {
     socket.onopen = () => {
       console.log('connected to websocket!')
@@ -13,12 +13,11 @@ export default function Home() {
   })
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {}
     </div>
   )
 }
