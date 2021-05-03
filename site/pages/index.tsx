8a39c97ae503061d37b3ab56b302246967ca6b25
Nick Baker <ndbaker1@outlook.com>
2021-05-02 23:55:23 -0500

    only call the health check on first load

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -5,11 +5,12 @@ import { Button, Paper, Slide, Snackbar, TextField } from '@material-ui/core'
 import { ServerEvent, ServerEventCodes } from 'utils/event-types'
 import { environment } from 'environment'
 
+// wake up app using the health endpoint
+if (environment.healthCheck) {
+  fetch(`${environment.http_or_https}://${environment.apiDomain}/health`).then(() => console.log('health check passed'))
+}
+
 export default function Home(): JSX.Element {
-  // wake up app using the health endpoint
-  if (environment.healthCheck) {
-    fetch(`${environment.http_or_https}://${environment.apiDomain}/health`).then(() => console.log('health check passed'))
-  }
   // Make sure to utilize <useRef()> because the object will be stale in closures
   const clientRef = useRef<ClientConnection>(
     new ClientConnection({
